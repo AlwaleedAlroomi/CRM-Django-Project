40524adcad518d0acac24f7e189db9af7301d486
Alwaleed Alroomi <alwaleed101az@gmail.com>
2023-09-27 00:20:49 +0300

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You logged in successfuly")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error try again")
+            return redirect('home')
+    else:
+        return render(request, template_name='home.html', context={})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You loggout successfuly")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, template_name='register.html', context={})
